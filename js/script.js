5ba79727d1082337c2859d540551576c0cebfb08
MatteoPaniccia <100145876+MatteoPaniccia@users.noreply.github.com>
2022-05-03 17:20:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.body.addEventListener("touchstart", playVideo);
+    function playVideo() {
+        const video = document.getElementById('myVideo');
+        if(video.playing) {
+        } else {
+            video.play();
+        }
+    }
+});
+
+
